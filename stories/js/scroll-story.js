babbd72b9a7bb1dc74cb130fac830807bc8cd66c
Don Button <donpublic@digitalgizmo.com>
2019-10-25 10:33:18 -0400

    image seq 4 start

--- stories/js/scroll-story.js
+++ stories/js/scroll-story.js
@@ -175,6 +175,36 @@ $("#caption-sequence2").children().each(function(i) {
 	} // end if i > 0
 });
 
+// Create scenes
+// Loop through all divs in text-panel
+// ----- Chapter 4 Designing South Mall ----
+$("#caption-sequence4").children().each(function(i) {
+	// Skip first image -- it's a pin, not a transition
+	if (i > 0) {
+		// Current target is +1 because 1st div in image panel is title
+		var targetPrev = imageSeqElems[2].children().eq(i);
+		let target = imageSeqElems[2].children().eq(i + 1);
+
+		var tl = new TimelineMax();
+		// Base transitin condition class name
+		// "this" is not a jQuery object, so need straight JS
+		tl.to(target, 2, {autoAlpha:1})
+			.to(targetPrev, 1, {autoAlpha:0});			
+
+		new ScrollMagic.Scene({
+			triggerElement: this,
+			duration: 350, // pin the element for a total of 400px
+			triggerHook: .8, // trigger low in the viewport
+		})
+		.setTween(tl)
+		// .on('start', function() {
+		// 	console.log("-- Chap2 targPrev: " + i + " target: " + (i+1));
+		// })
+		.addIndicators()
+		.addTo(ctrl);
+	} // end if i > 0
+});
+
 // --------- MAP -------------
 
 // Set pin for wide map
